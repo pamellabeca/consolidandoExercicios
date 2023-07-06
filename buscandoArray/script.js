2a742614ca12dcd7ddacba14b9854f5f6f757ac8
Pamella Silva <Pamellasilva296@gmail.com>
2023-07-06 00:21:06 -0300

    Criada função para contar categorias e seus respectivos números

---
+++ buscandoArray/script.js
@@ -0,0 +1,57 @@
+const booksByCategory = [
+    {
+        category: "Riqueza",
+        books: [
+            {
+                title: "Os segredos da mente milionária",
+                author: "T. Harv Eker",
+            },
+            {
+                title: "O homem mais rico da Babilônia",
+                author: "George S. Clason",
+            },
+            {
+                title: "Pai rico, pai pobre",
+                author: "Robert T. Kiyosaki e Sharon L. Lechter",
+            },
+        ],
+    },
+    {
+        category: "Inteligência Emocional",
+        books: [
+            {
+                title: "Você é Insubstituível",
+                author: "Augusto Cury",
+            },
+            {
+                title: "Ansiedade – Como enfrentar o mal do século",
+                author: "Augusto Cury",
+            },
+            {
+                title: "Os 7 hábitos das pessoas altamente eficazes",
+                author: "Stephen R. Covey",
+            },
+        ],
+    },
+];
+
+
+
+function contarCategoriasLivros(booksByCategory) {
+    let numcategoria = booksByCategory.length
+    console.log("Número de Categoria: " + numcategoria)
+    console.log("")
+
+    for (let i = 0; i < booksByCategory.length; i++) {
+      let categoria = booksByCategory[i].category
+      let numBooks = booksByCategory[i].books.length
+  
+      console.log("Categoria: " + categoria)
+      console.log("Número de livros: " + numBooks)
+      console.log("")
+    }
+}  
+
+
+
+contarCategoriasLivros(booksByCategory)
